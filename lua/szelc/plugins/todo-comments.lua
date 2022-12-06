e179e96b871c1b772ef48ded83b1715cfd474bf5
SzelamC <chanszelam7725@gmail.com>
2022-12-07 02:10:28 +0800

    test commit

--- lua/szelc/plugins/todo-comments.lua
+++ lua/szelc/plugins/todo-comments.lua
@@ -1,6 +1,27 @@
 local status, todo_comment = pcall(require, "todo-comments")
 if not status then
-  return
+	return
+end
+if not status then
+	if not status then
+		if not status then
+			if not status then
+				if not status then
+					if not status then
+						if not status then
+							return
+						end
+						return
+					end
+					return
+				end
+				return
+			end
+			return
+		end
+		return
+	end
+	return
 end
 
 todo_comment.setup()
